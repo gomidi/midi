dac086e808e5ade4a06a4a17887319e21d07ca06
Marc Rene Arns <arns@stridor.net>
2018-08-11 21:50:13 +0200

    add package docs for key and meter packages

---
+++ midimessage/meta/key/doc.go
@@ -0,0 +1,9 @@
+// Copyright (c) 2018 Marc René Arns. All rights reserved.
+// Use of this source code is governed by a MIT
+// license that can be found in the LICENSE file.
+
+/*
+	Package key provides helper functions for key signature meta messages.
+
+*/
+package key
