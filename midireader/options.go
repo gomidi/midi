83eaf07f775a524509151ac1e4da9ae01249148b
Marc Rene Arns <arns@stridor.net>
2018-09-03 21:51:09 +0200

    fix error in documentation of midireader NoteOffVelocity option

--- midireader/options.go
+++ midireader/options.go
@@ -6,7 +6,7 @@ type Option func(rd *reader)
 // NoteOffVelocity is an option for the reader that lets it differentiate between "fake" noteoff messages
 // (which are in fact noteon messages (typ 9) with velocity of 0) and "real" noteoff messages (typ 8)
 // having their own velocity.
-// The former are returned as NoteOffVelocity messages and keep the given velocity, the later
+// The later are returned as NoteOffVelocity messages and keep the given velocity, the former
 // are returned as NoteOff messages without velocity. That means in order to get all noteoff messages,
 // there must be checks for NoteOff and NoteOffVelocity (if this option is set).
 // If this option is not set, both kinds are returned as NoteOff (default).
