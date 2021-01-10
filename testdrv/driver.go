e4a62332b6e200652195b7cf5b08e21081ffff27
Marc Rene Arns <arns@stridor.net>
2021-01-10 14:17:40 +0100

    fix testdriver with locks in order to be able to run it with goroutines

--- testdrv/driver.go
+++ testdrv/driver.go
@@ -11,6 +11,9 @@ package testdrv
 import (
 	"io"
 
+	"sync"
+	"time"
+
 	"gitlab.com/gomidi/midi"
 )
 
@@ -19,12 +22,15 @@ type Driver struct {
 	out      *out
 	listener func([]byte, int64)
 	name     string
+	last     time.Time
+	mx       sync.Mutex
 }
 
 func New(name string) midi.Driver {
 	d := &Driver{name: name}
 	d.in = &in{name: name + "-in", driver: d, number: 0}
 	d.out = &out{name: name + "-out", driver: d, number: 0}
+	d.last = time.Now()
 	return d
 }
 
@@ -40,30 +46,45 @@ type in struct {
 	driver *Driver
 }
 
-func (f *in) StopListening() error    { f.driver.listener = nil; return nil }
+func (f *in) StopListening() error {
+	f.driver.mx.Lock()
+	f.driver.listener = nil
+	f.driver.mx.Unlock()
+	return nil
+}
 func (f *in) String() string          { return f.name }
 func (f *in) Number() int             { return f.number }
 func (f *in) IsOpen() bool            { return f.isOpen }
 func (f *in) Underlying() interface{} { return nil }
 
 func (f *in) SetListener(listener func([]byte, int64)) error {
+	f.driver.mx.Lock()
 	f.driver.listener = listener
+	f.driver.mx.Unlock()
 	return nil
 }
 func (f *in) Close() error {
+	f.driver.mx.Lock()
 	if !f.isOpen {
+		f.driver.mx.Unlock()
 		return nil
 	}
+	f.driver.mx.Unlock()
 	f.StopListening()
+	f.driver.mx.Lock()
 	f.isOpen = false
+	f.driver.mx.Unlock()
 	return nil
 }
 
 func (f *in) Open() error {
+	f.driver.mx.Lock()
 	if f.isOpen {
+		f.driver.mx.Unlock()
 		return nil
 	}
 	f.isOpen = true
+	f.driver.mx.Unlock()
 	return nil
 }
 
@@ -80,27 +101,41 @@ func (f *out) String() string          { return f.name }
 func (f *out) Underlying() interface{} { return nil }
 
 func (f *out) Close() error {
+	f.driver.mx.Lock()
 	if !f.isOpen {
+		f.driver.mx.Unlock()
 		return nil
 	}
 	f.isOpen = false
+	f.driver.mx.Unlock()
 	return nil
 }
 func (f *out) Write(b []byte) (int, error) {
+	f.driver.mx.Lock()
 	if !f.isOpen {
+		f.driver.mx.Unlock()
 		return 0, midi.ErrPortClosed
 	}
 	if f.driver.listener == nil {
+		f.driver.mx.Unlock()
 		return 0, io.EOF
 	}
-	f.driver.listener(b, 0)
+
+	now := time.Now()
+	dur := now.Sub(f.driver.last)
+	f.driver.last = now
+	f.driver.listener(b, dur.Microseconds())
+	f.driver.mx.Unlock()
 	return len(b), nil
 }
 
 func (f *out) Open() error {
+	f.driver.mx.Lock()
 	if f.isOpen {
+		f.driver.mx.Unlock()
 		return nil
 	}
 	f.isOpen = true
+	f.driver.mx.Unlock()
 	return nil
 }
