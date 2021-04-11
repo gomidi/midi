320ba8353999b1be88c4a418b59c3514993c40fa
Alan Zaffetti <ajzaff@gmail.com>
2021-04-11 04:14:08 +0000

    Cleanup: Update midimessage/channel/channel.Channel to use iota.

--- midimessage/channel/channel.go
+++ midimessage/channel/channel.go
@@ -1,54 +1,57 @@
 package channel
 
-// TODO do with iota
+// Channel represents a MIDI channel
+// there must not be more than 16 MIDI channels (0-15)
+type Channel uint8
+
 const (
 	// MIDI channel 1
-	Channel0 = Channel(0)
+	Channel0 Channel = iota
 
 	// MIDI channel 2
-	Channel1 = Channel(1)
+	Channel1
 
 	// MIDI channel 3
-	Channel2 = Channel(2)
+	Channel2
 
 	// MIDI channel 4
-	Channel3 = Channel(3)
+	Channel3
 
 	// MIDI channel 5
-	Channel4 = Channel(4)
+	Channel4
 
 	// MIDI channel 6
-	Channel5 = Channel(5)
+	Channel5
 
 	// MIDI channel 7
-	Channel6 = Channel(6)
+	Channel6
 
 	// MIDI channel 8
-	Channel7 = Channel(7)
+	Channel7
 
 	// MIDI channel 9
-	Channel8 = Channel(8)
+	Channel8
 
 	// MIDI channel 10
-	Channel9 = Channel(9)
+	Channel9
 
 	// MIDI channel 11
-	Channel10 = Channel(10)
+	Channel10
 
 	// MIDI channel 12
-	Channel11 = Channel(11)
+	Channel11
 
 	// MIDI channel 13
-	Channel12 = Channel(12)
+	Channel12
 
 	// MIDI channel 14
-	Channel13 = Channel(13)
+	Channel13
 
 	// MIDI channel 15
-	Channel14 = Channel(14)
+	Channel14
 
 	// MIDI channel 16
-	Channel15 = Channel(15)
+	Channel15
 )
 
 func SetChannel(msg Message, ch uint8) Message {
@@ -79,10 +82,6 @@ func SetChannel(msg Message, ch uint8) Message {
 
 	panic("unreachable")
 }
-
-// Channel represents a MIDI channel
-// there must not be more than 16 MIDI channels (0-15)
-type Channel uint8
 
 // Channel returns the number of the MIDI channel (0-15)
 func (c Channel) Channel() uint8 {
