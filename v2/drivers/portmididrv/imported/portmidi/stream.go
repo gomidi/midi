6b2d6be57613f04887eeb5dc708e1ea6ac6911b5
Marc Rene Arns <arns@stridor.net>
2022-04-29 13:01:08 +0200

    cleanup portmidi

--- v2/drivers/portmididrv/imported/portmidi/stream.go
+++ v2/drivers/portmididrv/imported/portmidi/stream.go
@@ -358,23 +358,6 @@ func (s *Stream) Poll() (bool, error) {
 	return poll > 0, nil
 }
 
-// TODO: add bindings for Pm_SetFilter
-
-/*
-\function PmError Pm_SetFilter( PortMidiStream* stream, int32_t filters )
-    Pm_SetFilter() sets filters on an open input stream to drop selected
-    input types. By default, only active sensing messages are filtered.
-    To prohibit, say, active sensing and sysex messages, call
-    Pm_SetFilter(stream, PM_FILT_ACTIVE | PM_FILT_SYSEX);
-
-    Filtering is useful when midi routing or midi thru functionality is being
-    provided by the user application.
-    For example, you may want to exclude timing messages (clock, MTC, start/stop/continue),
-    while allowing note-related messages to pass.
-    Or you may be using a sequencer or drum-machine for MIDI clock information but want to
-    exclude any notes it may play.
-*/
-
 type Filter int
 
 var FILTER_ACTIVE = C.PM_FILT_ACTIVE
@@ -398,52 +381,6 @@ var FILTER_SONG_POSITION = C.PM_FILT_SONG_POSITION
 var FILTER_SONG_SELECT = C.PM_FILT_SONG_SELECT
 var FILTER_TUNE = C.PM_FILT_TUNE
 var FILTER_SYSTEMCOMMON = C.PM_FILT_SYSTEMCOMMON
-
-/*
-// filter active sensing messages (0xFE):
-#define PM_FILT_ACTIVE (1 << 0x0E)
-// filter system exclusive messages (0xF0):
-#define PM_FILT_SYSEX (1 << 0x00)
-// filter MIDI clock message (0xF8)
-#define PM_FILT_CLOCK (1 << 0x08)
-// filter play messages (start 0xFA, stop 0xFC, continue 0xFB)
-#define PM_FILT_PLAY ((1 << 0x0A) | (1 << 0x0C) | (1 << 0x0B))
-// filter tick messages (0xF9)
-#define PM_FILT_TICK (1 << 0x09)
-// filter undefined FD messages
-#define PM_FILT_FD (1 << 0x0D)
-// filter undefined real-time messages
-#define PM_FILT_UNDEFINED PM_FILT_FD
-// filter reset messages (0xFF)
-#define PM_FILT_RESET (1 << 0x0F)
-// filter all real-time messages
-#define PM_FILT_REALTIME (PM_FILT_ACTIVE | PM_FILT_SYSEX | PM_FILT_CLOCK | \
-    PM_FILT_PLAY | PM_FILT_UNDEFINED | PM_FILT_RESET | PM_FILT_TICK)
-// filter note-on and note-off (0x90-0x9F and 0x80-0x8F
-#define PM_FILT_NOTE ((1 << 0x19) | (1 << 0x18))
-// filter channel aftertouch (most midi controllers use this) (0xD0-0xDF)
-#define PM_FILT_CHANNEL_AFTERTOUCH (1 << 0x1D)
-// per-note aftertouch (0xA0-0xAF)
-#define PM_FILT_POLY_AFTERTOUCH (1 << 0x1A)
-// filter both channel and poly aftertouch
-#define PM_FILT_AFTERTOUCH (PM_FILT_CHANNEL_AFTERTOUCH | PM_FILT_POLY_AFTERTOUCH)
-// Program changes (0xC0-0xCF)
-#define PM_FILT_PROGRAM (1 << 0x1C)
-// Control Changes (CC's) (0xB0-0xBF)
-#define PM_FILT_CONTROL (1 << 0x1B)
-// Pitch Bender (0xE0-0xEF
-#define PM_FILT_PITCHBEND (1 << 0x1E)
-// MIDI Time Code (0xF1)
-#define PM_FILT_MTC (1 << 0x01)
-// Song Position (0xF2)
-#define PM_FILT_SONG_POSITION (1 << 0x02)
-// Song Select (0xF3)
-#define PM_FILT_SONG_SELECT (1 << 0x03)
-// Tuning request (0xF6)
-#define PM_FILT_TUNE (1 << 0x06)
-// All System Common messages (mtc, song position, song select, tune request)
-#define PM_FILT_SYSTEMCOMMON (PM_FILT_MTC | PM_FILT_SONG_POSITION | PM_FILT_SONG_SELECT | PM_FILT_TUNE)
-*/
 
 func (s *Stream) SetFilter(filters int) error {
 	filter := C.Pm_SetFilter(unsafe.Pointer(s.pmStream), C.int32_t(filters))
