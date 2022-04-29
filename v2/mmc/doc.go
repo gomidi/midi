c5220720f40c4cc9a8fb5501b8bcd66c9a41d4c7
Marc Rene Arns <arns@stridor.net>
2022-04-29 12:05:38 +0200

    add doc for mmc package

---
+++ v2/mmc/doc.go
@@ -0,0 +1,10 @@
+// Copyright (c) 2022 Marc René Arns. All rights reserved.
+// Use of this source code is governed by a MIT
+// license that can be found in the LICENSE file.
+
+/*
+Package mmc helps with reading and writing of MIDI Universal Real Time SysEx Commands.
+
+
+*/
+package mmc
