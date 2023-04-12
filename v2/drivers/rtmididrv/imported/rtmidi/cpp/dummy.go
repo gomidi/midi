2097a7cec21cf5b1e1990c7951c1e4ce41719109
Marc Rene Arns <linux@marcrenearns.de>
2023-04-12 16:06:34 -0400

    add dummy go file to fix https://gitlab.com/gomidi/midi/-/issues/38

---
+++ v2/drivers/rtmididrv/imported/rtmidi/cpp/dummy.go
@@ -0,0 +1,7 @@
+package cpp
+
+// dummy go file to help go mod vendor
+// to avoid accidentally deleting the critical cpp directory
+// a suggested by Andrew Pennebaker, see https://gitlab.com/gomidi/midi/-/issues/38
+
+import "C"
